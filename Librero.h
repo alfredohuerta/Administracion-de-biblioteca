a44fa377c6a2ad2990aac63361a61cba8bb973ea
Alfredo Huerta <74021668+alfredohuerta@users.noreply.github.com>
2021-05-28 00:47:13 -0500

    Create Librero.h
    La clase Librero almacena la clase fila, se espera que almacene simultáneamente 6 filas, pero para demostrar su funcionamiento se ha creado sólamente una fila. Más filas serán agregadas en futuras versiones.

---
+++ Librero.h
@@ -0,0 +1,57 @@
+#ifndef LIBRERO_H
+#define LIBRERO_H
+
+#include <iostream>
+#include <string>
+
+#include "Ficha.h"
+#include "Libro.h"
+#include "Fila.h"
+
+using namespace std;
+
+const int maxFilas= 6;
+
+class Librero 
+{
+    public:
+        Librero(int id, string sector);
+        ~Librero(){
+            for (int i = 0; i < noFila; i++){
+                delete filas[i];
+            }
+        };
+        virtual void llenarLibrero();
+        virtual void mostrarDatosLibrero();
+
+    private:
+        string seccion;
+        int noLibrero;
+        int noFila;
+        int capacidad;
+        Fila *filas[maxFilas];
+
+};
+
+Librero :: Librero(int id, string sector)
+: noLibrero(id), seccion(sector)
+{
+    noFila= 0;
+    *filas[noFila];
+    capacidad= 6;
+}
+
+void Librero :: llenarLibrero(){
+    filas[noFila]= new Fila(1);
+    filas[noFila]->llenarFila(); 
+    noFila++;
+}
+
+void Librero :: mostrarDatosLibrero(){
+    cout << "Sección: " << seccion << endl;
+    for (int i = 0; i < noFila; i++){
+        filas[i]->mostrarDatos();
+    }
+}
+
+#endif
