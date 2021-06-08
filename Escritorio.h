1d083e7eec2cf024959fd17440853be52300bc48
Alfredo Huerta <74021668+alfredohuerta@users.noreply.github.com>
2021-06-07 20:56:55 -0500

    Update Escritorio.h
    Se agregaron comentarios al código, además de los métodos getDatosComputadora, usarComputadora y getComputo

--- Escritorio.h
+++ Escritorio.h
@@ -31,12 +31,16 @@ class Escritorio
         virtual void asignarCompu();
         virtual void solicitarAyuda();
         void datosEscritorio();
+        bool getComputo();
+        void setDesocupado();
+        void usarComputadora();
+        void getDatosComputadora();
     private:
-    Computadora *compus[maxComp];
-    string estado;
-    int idEscritorio;
-    bool computadora;
-    int noComputadora;
+        Computadora *compus[1];
+        string estado;
+        int idEscritorio;
+        bool computadora;
+        int noComputadora;
 };
 
 Escritorio :: Escritorio(int idEsc, bool compu){
@@ -51,6 +55,10 @@ void Escritorio :: ocuparEscritorio(){
     cout << "El escritorio se encuentra " << estado << " por usted." << endl;
 }
 
+void Escritorio :: setDesocupado(){
+    estado= "desocupado";
+}
+
 //Validación por medio de un if de la presencia de computadoras
 void Escritorio :: computo(){
     if (computadora == true){
@@ -60,7 +68,10 @@ void Escritorio :: computo(){
     }
 }
 
-//Creación de computadora dentro de la clase escritorio, no todos los escritorios tienen computadoras, así que debe validar.
+/*
+* Creación de objeto computadora dentro de la clase escritorio, no todos los escritorios 
+* tienen computadoras, así que debe validar primero que el valor booleano sea verdadero
+*/
 void Escritorio :: asignarCompu(){
     if (computadora == true){
         compus[noComputadora]= new Computadora(noComputadora);
@@ -80,10 +91,27 @@ void Escritorio :: datosEscritorio(){
     } else{
         cout << "No tiene computadora." << endl;
     }
+    cout << "\n";
 }
 
+/*
+* Esta función sólo apunta a la función solApoyo del objeto computadora.
+*/
 void Escritorio :: solicitarAyuda(){
     compus[noComputadora]->solApoyo();
+}
+
+bool Escritorio :: getComputo(){
+    return computadora;
+}
+
+void Escritorio :: usarComputadora(){
+    cout << "Holi";
+}
+
+//Esta función apunta a la función getDatos del objeto computadora.
+void Escritorio :: getDatosComputadora(){
+    compus[noComputadora]->getDatos();
 }
 
 #endif
