3c1778a68ba787cab666fc28c9dd4e20aab3219a
Alfredo Huerta <74021668+alfredohuerta@users.noreply.github.com>
2021-06-11 04:29:06 -0500

    Update Librero.h

--- Librero.h
+++ Librero.h
@@ -1,111 +1,149 @@
 /*
-    Erick Alfredo García Huerta
-    A01708119
-    Proyecto Administración de una biblioteca Clase Librero
-    Esta clase crea las filas donde se almacenarán las filas y se mostrarán sus datos.
+* Erick Alfredo García Huerta
+* A01708119
+* 
+* Proyecto Administración de Biblioteca -> clase Librero
+* 
+* La clase Librero almacena las filas que a su vez almacenan los textos. Ésta recibe un número de identificación y una 
+* temática a cada librero para facilitar la navegación. Además incluye una función que permite construir fila a fila
+* el librero e irlo llenando de libros.
 */
 
 #ifndef LIBRERO_H
 #define LIBRERO_H
 
-#include <iostream>
+#include <stdio.h>
 #include <string>
+#include <iostream>
 
-#include "Ficha.h"
-#include "Libro.h"
+#include "Texto.h"
 #include "Fila.h"
 
 using namespace std;
 
 const int maxFilas= 6;
 
-class Librero 
-{
-    public:
-        Librero(int id, string sector);
-        Librero();
-        //Destructor de objetos
-        ~Librero(){
-            for (int i = 0; i < noFila; i++){
-                delete filas[i];
-            }
-        };
-        virtual void llenarLibrero();
-        virtual void mostrarDatosLibrero();
-        virtual void getPrestarLibros();
-        virtual void getPrestarRevistas();
-        virtual void getDatosFila();
-        void getLibreros();
-
+class Librero{
     private:
         string seccion;
         int noLibrero;
         int noFila;
-        int capacidad;
         Fila *filas[maxFilas];
-
+    public:
+        Librero();
+        Librero(int numeroLibrero, string nombreSeccion);
+        ~Librero(){
+            for(int i= 0; i<maxFilas; i++){
+                delete filas[i];
+            }
+        }
+        void crearEjemplos();
+        void setPrestar();
+        void getLibrosFila();
+        void getRevistasFila();
+        void getDisponiblesFila();
+        void getPrestadosFila();
+        void getDatos();
+        void crearFila();
 };
 
-Librero :: Librero(int id, string sector)
-: noLibrero(id), seccion(sector)
-{
+Librero :: Librero(){
+    seccion= "";
     noFila= 0;
-    *filas[noFila];
-    capacidad= 6;
-}
+};
+
+Librero :: Librero(int numeroLibrero, string nombreSeccion){
+    noLibrero= numeroLibrero;
+    seccion= nombreSeccion;
+    noFila= 0;
+};
 
 /*
-* Función que devuelve los datos del librero en la  clase Biblioteca
+* crearEjemplos() crea una única fila con cuatro textos en su interior, esto sirve como caso de prueba y es una forma 
+* rápida de aplicar las funciones.
+* @param: 
+* @return: fila.
 */
-void Librero :: getLibreros(){
-    cout << "Sección: " << seccion << endl;
-    cout << "Número de librero: " << noLibrero << endl;
-}
-
-/*
-* Esta función genera objetos tipo Fila y objetos tipo Libro que se asignarán al arreglo de la fila
-* para modificar los libros que aparecen, se deben modificar el contructor en la clase fila
-*/
-void Librero :: llenarLibrero(){
+void Librero :: crearEjemplos(){
     filas[noFila]= new Fila(noFila);
-    filas[noFila]->llenarFila(); 
+    filas[noFila]->crearEjemplos();
     noFila++;
 }
 
-/*
-* Esta función muestra los datos que se almacenan en el librero mediante un ciclo for
-* sólo sirve para visualizar en pantalla lo que almacena la fila.
-*/
-void Librero :: mostrarDatosLibrero(){
-    cout << "Sección: " << seccion << endl;
-    for (int i = 0; i < noFila; i++){
-        filas[i]->mostrarDatos();
+void Librero :: setPrestar(){
+    int idFila;
+    cout << "Ingrese el numero de fila al que quiere acceder: ";
+    cin >> idFila;
+    filas[idFila]->setPrestados();
+}
+
+void Librero :: getLibrosFila(){
+    int idFila;
+    cout << "Ingrese el numero de fila al que quiere acceder: ";
+    cin >> idFila;
+    filas[idFila]->getLibros();
+}
+
+void Librero :: getRevistasFila(){
+    int idFila;
+    cout << "Ingrese el numero de fila al que quiere acceder: ";
+    cin >> idFila;
+    filas[idFila]->getRevistas();
+}
+
+void Librero :: getDisponiblesFila(){
+    int idFila;
+    cout << "Ingrese el numero de fila al que quiere acceder: ";
+    cin >> idFila;
+    filas[idFila]->getDisponibles();
+}
+
+void Librero :: getPrestadosFila(){
+    int idFila;
+    cout << "Ingrese el numero de fila al que quiere acceder: ";
+    cin >> idFila;
+    filas[idFila]->getPrestados();
+}
+
+void Librero :: getDatos(){
+    cout << "Numero de librero: " << noLibrero << endl;
+    cout << "Tematica de la seccion: " << seccion << endl;
+    cout << "\n"; 
+    for(int i= 0 ; i<noFila ; i++){
+        filas[i]->getDatos();
     }
 }
 
 /*
-* Esta función es un intermediario para mostrar los datos específicos de un libro.
-* Apunta a un apuntador en la clase libro.
+* crearFila se lleva a cabo a través de un ciclo while que permite crear múltiples filas, a su vez, conecta con la  
+* función crearLibro() de Texto.h, para crear libros y revistas que se almacenarán automáticamente en la fila, al final
+* de cada ciclo se le permite al usuario terminarlo antes o se acaba automáticamente una vez el número de filas alcanza
+* el límite permitido, esto último como medida de seguridad.
+* 
+* param: 
+* @return: fila[], textos[]
 */
-void Librero :: getDatosFila(){
-    int fila;
-    cout << "Ingrese la fila: ";
-    cin >> fila;
-    filas[fila]->mostrarDatosIndv();
-}
+void Librero :: crearFila(){
+    bool flag= true;
+    int continuar;
 
-void Librero :: getPrestarLibros(){
-    int fila;
-    cout << "Ingrese el número de fila: ";
-    cin >> fila;
-    filas[fila]->getPrestarLibros();
-}
+    const char *str1= "Bienvenido al llenado del filas\n"
+                        "ADVERTENCIA! Se iniciara automaticamente el sistema de registro de textos, por favor ingrese \n"
+                        "al menos un texto nuevo en la fila hasta un maximo de 10";
+    puts(str1);
 
-void Librero :: getPrestarRevistas(){
-    int fila;
-    cout << "Ingrese el número de fila: ";
-    cin >> fila;
-    filas[fila]->getPrestarRevista();
+    while (flag)
+    {
+        filas[noFila]= new Fila(noFila);
+        filas[noFila]->crearLibros();
+        noFila++;
+
+        cout << "Dese llenar otra fila? INGRESE 1 para continuar, INGRESE 2 para terminar: ";
+        cin >> continuar;
+        if (continuar == 2){
+            flag= false;
+        }
+    }
 }
 
 #endif
